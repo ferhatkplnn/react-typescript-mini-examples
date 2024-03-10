c910f60d1ad6584fd11fe748a8a8bc5f0cdf10a1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-10 23:27:11 +0300

    Update the header and course goal components and styles.

--- your-goal-react-typescript-app/src/App.tsx
+++ your-goal-react-typescript-app/src/App.tsx
@@ -1,8 +1,13 @@
 import CourseGoal from "./components/CourseGoal";
+import goalImg from "./assets/images/goal.png";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <main>
+      <Header image={{ src: goalImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal title="Learn React + TS" id={1}>
         <p>Learn it from the ground up</p>
       </CourseGoal>
