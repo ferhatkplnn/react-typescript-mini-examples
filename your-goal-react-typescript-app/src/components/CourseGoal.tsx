71a61d47960ba82aef3ff83f1f91871abc72ec70
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-10 23:11:10 +0300

    Convert CourseGoal to a functional component

--- your-goal-react-typescript-app/src/components/CourseGoal.tsx
+++ your-goal-react-typescript-app/src/components/CourseGoal.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { FC, PropsWithChildren } from "react";
 
 // type CourseGoalProps = {
 //   title: string;
@@ -8,7 +8,21 @@ import { PropsWithChildren } from "react";
 
 type CourseGoalProps = PropsWithChildren<{ title: string; id: number }>;
 
-const CourseGoal = ({ title, children }: CourseGoalProps) => {
+// const CourseGoal = ({ title, children }: CourseGoalProps) => {
+//   return (
+//     <article className="card">
+//       <div className="card__content">
+//         <h2 className="card__content__title">{title}</h2>
+//         <p className="card__content__description">{children}</p>
+//       </div>
+//       <button className="card__button">Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
+const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
   return (
     <article className="card">
       <div className="card__content">
