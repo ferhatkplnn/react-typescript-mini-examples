adff7187b9ccaf6e7dbac76dadc7c28cbc3dd9f7
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 13:29:26 +0300

    feat: enhance Form component with onSave callback

--- scribble-2/src/components/Form.tsx
+++ scribble-2/src/components/Form.tsx
@@ -1,9 +1,22 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, FormEvent } from "react";
 
-type TFromProps = ComponentPropsWithoutRef<"form">;
+type TFormProps = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
 
-const Form = (props: TFromProps) => {
-  return <form {...props}>{props.children}</form>;
+const Form = ({ onSave, children, ...otherProps }: TFormProps) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+  };
+  return (
+    <form onSubmit={handleSubmit} {...otherProps}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
