f22d1ed69ee56ac73b92cf2b6575435b4db9d21a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 13:07:38 +0300

    feat: Introduce useRef for age input in App component

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -1,12 +1,15 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 const App = () => {
+  const input = useRef<HTMLInputElement | null>(null);
+
   return (
     <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
       <Input id="name" label="Your name" type="text" />
-      <Input id="name" label="Your age" type="number" />
+      <Input id="name" label="Your age" type="number" ref={input} />
       <Button />
       <Container as={"code"}>Hello there</Container>
     </main>
