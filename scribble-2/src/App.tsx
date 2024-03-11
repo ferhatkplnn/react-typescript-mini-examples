61b6d6f113b923b2f1c622ec393e524574c5c493
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 13:14:18 +0300

    feat: Introduce Form component and update App layout

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -2,15 +2,19 @@ import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import Form from "./components/Form";
 
 const App = () => {
   const input = useRef<HTMLInputElement | null>(null);
 
   return (
     <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
-      <Input id="name" label="Your name" type="text" />
-      <Input id="name" label="Your age" type="number" ref={input} />
-      <Button />
+      <Form>
+        <Input id="name" label="Your name" type="text" />
+        <Input id="name" label="Your age" type="number" ref={input} />
+        <Button>Save</Button>
+      </Form>
+
       <Container as={"code"}>Hello there</Container>
     </main>
   );
