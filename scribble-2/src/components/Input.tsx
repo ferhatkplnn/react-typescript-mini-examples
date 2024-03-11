52fe80555348787c12217faa92784e16a8c3dbb3
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 13:07:11 +0300

    refactor: Update Input component to use forwardRef for improved reference handling

--- scribble-2/src/components/Input.tsx
+++ scribble-2/src/components/Input.tsx
@@ -1,23 +1,26 @@
-import { ComponentProps } from "react";
+import { ComponentPropsWithRef, forwardRef } from "react";
 
 type TInputProps = {
   label: string;
   id: string;
-} & ComponentProps<"input">;
+} & ComponentPropsWithRef<"input">;
 
-const Input = ({ label, id, ...props }: TInputProps) => {
-  return (
-    <div className="flex flex-col space-y-1">
-      <label htmlFor={id} className="uppercase font-semibold text-sm">
-        {label}
-      </label>
-      <input
-        id={id}
-        className="text-black text-lg px-4 py-2 rounded"
-        {...props}
-      />
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, TInputProps>(
+  ({ label, id, ...props }, ref) => {
+    return (
+      <div className="flex flex-col space-y-1">
+        <label htmlFor={id} className="uppercase font-semibold text-sm">
+          {label}
+        </label>
+        <input
+          id={id}
+          className="text-black text-lg px-4 py-2 rounded"
+          {...props}
+          ref={ref}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
