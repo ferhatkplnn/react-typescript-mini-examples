c1e69cab44c5b64dcb22bf0413a5dbdb41b6be0a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 11:10:26 +0300

    feat: Add conditional rendering for goal list with information boxes
    - Display a hit box when there are no course goals
    - Show a warning box if there are 4 or more goals
    - Improve user experience for goal tracking

--- your-goal-react-typescript-app/src/components/CouserGoalList.tsx
+++ your-goal-react-typescript-app/src/components/CouserGoalList.tsx
@@ -1,6 +1,8 @@
 import { Goal } from "../types";
 import CourseGoal from "./CourseGoal";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalList = {
   goals: Goal[];
@@ -10,19 +12,40 @@ type CourseGoalList = {
 const CouserGoalList = ({ goals, handleDeleteGoal }: CourseGoalList) => {
   const [parent] = useAutoAnimate();
 
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <div ref={parent} className="goals">
-      {goals.map((goal) => (
-        <CourseGoal
-          handleDeleteGoal={handleDeleteGoal}
-          title={goal.title}
-          id={goal.id}
-          key={goal.id}
-        >
-          {goal.descrtiption}
-        </CourseGoal>
-      ))}
-    </div>
+    <>
+      {warningBox}
+      <div ref={parent} className="goals">
+        {goals.map((goal) => (
+          <CourseGoal
+            handleDeleteGoal={handleDeleteGoal}
+            title={goal.title}
+            id={goal.id}
+            key={goal.id}
+          >
+            {goal.descrtiption}
+          </CourseGoal>
+        ))}
+      </div>
+    </>
   );
 };
 
