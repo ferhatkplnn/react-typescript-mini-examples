b46cd1a1b496c2bcebfca73aa5222154147b57d8
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 13:30:00 +0300

    feat: integrate Form component with onSave callback in App

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -7,9 +7,14 @@ import Form from "./components/Form";
 const App = () => {
   const input = useRef<HTMLInputElement | null>(null);
 
+  const handleSave = (data: unknown) => {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  };
+
   return (
     <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
-      <Form>
+      <Form onSave={handleSave}>
         <Input id="name" label="Your name" type="text" />
         <Input id="name" label="Your age" type="number" ref={input} />
         <Button>Save</Button>
