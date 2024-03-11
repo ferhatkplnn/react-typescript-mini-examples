d7152fa9dbcbefc4126781f575b13ab18ef2dac4
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 12:59:01 +0300

    feat: Enhance App component with Button and Container components

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -1,3 +1,5 @@
+import Button from "./components/Button";
+import Container from "./components/Container";
 import Input from "./components/Input";
 
 const App = () => {
@@ -5,6 +7,8 @@ const App = () => {
     <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
       <Input id="name" label="Your name" type="text" />
       <Input id="name" label="Your age" type="number" />
+      <Button />
+      <Container as={"code"}>Hello there</Container>
     </main>
   );
 };
