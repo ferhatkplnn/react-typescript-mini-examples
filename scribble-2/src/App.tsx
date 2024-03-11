0ddc1dddb26339bf6172370ae1fc5c8806230c43
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 12:20:57 +0300

    feat: Add input types to App component

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -3,8 +3,8 @@ import Input from "./components/Input";
 const App = () => {
   return (
     <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
-      <Input id="name" label="Your name" />
-      <Input id="name" label="Your age" />
+      <Input id="name" label="Your name" type="text" />
+      <Input id="name" label="Your age" type="number" />
     </main>
   );
 };
