720d54bc41836bfd53cca03709d460026140ea12
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 12:21:38 +0300

    refactor: Add ComponentProps<"input"> to Input component

--- scribble-2/src/components/Input.tsx
+++ scribble-2/src/components/Input.tsx
@@ -1,9 +1,11 @@
+import { ComponentProps } from "react";
+
 type TInputProps = {
   label: string;
   id: string;
-};
+} & ComponentProps<"input">;
 
-const Input = ({ label, id }: TInputProps) => {
+const Input = ({ label, id, ...props }: TInputProps) => {
   return (
     <div className="flex flex-col space-y-1">
       <label htmlFor={id} className="uppercase font-semibold text-sm">
@@ -11,8 +13,8 @@ const Input = ({ label, id }: TInputProps) => {
       </label>
       <input
         id={id}
-        type="text"
         className="text-black text-lg px-4 py-2 rounded"
+        {...props}
       />
     </div>
   );
