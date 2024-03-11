eadb0c153ea85ea2046702575d6bfd75a04ef071
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 10:59:47 +0300

    feat: Add HTML structure to InfoBox component

--- your-goal-react-typescript-app/src/components/InfoBox.tsx
+++ your-goal-react-typescript-app/src/components/InfoBox.tsx
@@ -5,8 +5,21 @@ type InfoBoxProps = {
   children: ReactNode;
 };
 
-const InfoBox = () => {
-  return <div>InfoBox</div>;
+const InfoBox = ({ mode, children }: InfoBoxProps) => {
+  if (mode === "hint") {
+    return (
+      <aside className="infobox">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+
+  return (
+    <aside className="infobox infobox-warning">
+      <h2>Warning</h2>
+      <p>{children}</p>
+    </aside>
+  );
 };
 
 export default InfoBox;
