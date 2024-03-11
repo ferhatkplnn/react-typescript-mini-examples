64e902e73d7f21529f2663e7d8856c2cc1fdd0b1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 11:51:24 +0300

    feat: Input component updated for semantic structure

--- scribble-2/src/components/Input.tsx
+++ scribble-2/src/components/Input.tsx
@@ -1,5 +1,15 @@
-const Input = () => {
-  return <div>Input</div>;
+type TInputProps = {
+  label: string;
+  id: string;
+};
+
+const Input = ({ label, id }: TInputProps) => {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
 };
 
 export default Input;
