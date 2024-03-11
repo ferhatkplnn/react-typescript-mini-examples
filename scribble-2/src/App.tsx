108966b189b5c55d4b91b278fa3420b50ca51069
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-11 12:09:18 +0300

    style: Apply styles to App component and Input elements

--- scribble-2/src/App.tsx
+++ scribble-2/src/App.tsx
@@ -2,7 +2,7 @@ import Input from "./components/Input";
 
 const App = () => {
   return (
-    <main>
+    <main className="bg-fuchsia-900 text-fuchsia-50 p-8 rounded drop-shadow-xl flex flex-col space-y-4 w-screen max-w-screen-sm">
       <Input id="name" label="Your name" />
       <Input id="name" label="Your age" />
     </main>
