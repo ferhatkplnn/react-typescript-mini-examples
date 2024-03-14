2efa4a6b2df03abbc1d88a292af484c4ca5222d3
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-14 17:27:34 +0300

    refactor: Simplify button logic in Header component
    - Refactored button logic to improve readability and maintainability
    - Consolidated button label and click handler assignment for clarity

--- timer-react-typescript/src/components/Header.tsx
+++ timer-react-typescript/src/components/Header.tsx
@@ -2,20 +2,17 @@ import Button from "./UI/Button";
 import { useTimersContext } from "../store/timersContext";
 
 const Header = () => {
-  const timersCtx = useTimersContext();
+  const { isRunning, startTimers, stopTimers } = useTimersContext();
+
+  const handleButtonClick = isRunning ? stopTimers : startTimers;
+  const buttonLabel = isRunning ? "Stop" : "Start";
 
   return (
     <header>
       <h1>ReactTimer</h1>
 
       <div>
-        <Button
-          onClick={
-            timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers
-          }
-        >
-          {timersCtx.isRunning ? "Stop" : "Start"} Timers
-        </Button>
+        <Button onClick={handleButtonClick}>{buttonLabel} Timers</Button>
       </div>
     </header>
   );
