16035e06b64243384e88bde79801a20c3ada5eaf
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-14 16:33:26 +0300

    feat: Add countdown functionality to Timer component
    - Implemented countdown feature using useState and useEffect hooks
    - Added progress bar to visually represent remaining time
    - Display remaining time in seconds with two decimal places

--- timer-react-typescript/src/components/Timer.tsx
+++ timer-react-typescript/src/components/Timer.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useRef, useState } from "react";
 import { type TTimer } from "../store/timersContext";
 import Container from "./UI/Container";
 
 type TTimerProps = TTimer;
 
 const Timer = ({ name, duration }: TTimerProps) => {
+  const [remaining, setRemaining] = useState(duration * 1000);
+  const intervalRef = useRef<number | null>(null);
+
+  if (remaining <= 0 && intervalRef.current) {
+    clearInterval(intervalRef.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemaining((prevTime) => prevTime - 50);
+    }, 50);
+
+    intervalRef.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remaining / 1000).toFixed(2);
+  console.log(formattedRemainingTime);
+
   return (
     <Container as="article">
       <h3>{name}</h3>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remaining} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 };
