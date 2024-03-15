1fa58a7f097eca5ee53b6dc6018121731256357f
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-15 12:34:04 +0300

    feat: Create cart item quantity management
    - Create addToCart and removeFromCart reducers for better quantity handling
    - Create readability and efficiency in managing cart item quantities

---
+++ scribble-redux-shop/src/store/cartSlice.ts
@@ -0,0 +1,45 @@
+import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+};
+
+type CartState = {
+  items: CartItem[];
+};
+
+const initialState: CartState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<Omit<CartItem, "quantitiy">>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity <= 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
+  },
+});
